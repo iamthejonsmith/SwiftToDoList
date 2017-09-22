ecc49d0039102a2f0b72da8b53f4983d16082370
Jon Smith <iamthejonsmith@hotmail.com>
2017-09-21 22:22:04 -0500

    Update to all files
    Upgrade to swift 3

--- SwiftToDoList/TableViewCell.swift
+++ SwiftToDoList/TableViewCell.swift
@@ -2,8 +2,8 @@
 //  TableViewCell.swift
 //  SwiftToDoList
 //
-//  Created by Jon Smith on 8/14/15.
-//  Copyright (c) 2015 Jon Smith. All rights reserved.
+//  Created by Jon Smith on 9/21/17.
+//  Copyright (c) 2017 Jon Smith. All rights reserved.
 //
 
 import UIKit
